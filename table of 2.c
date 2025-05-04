0c8d2a4296cfa3109bfd15003efb9d0695522476
Chaitanya11092006 <chaitanyameshram119@gmail.com>
2025-05-04 21:33:43 +0530

    Create table of 2.c

---
+++ table of 2.c
@@ -0,0 +1,14 @@
+Name: Chaitanya Pankaj Meshram PRN:24030331372010
+#include <stdio.h>
+
+int main() { 
+
+  int i;
+
+   for (i = 1; i <= 10; i++) 
+   { printf("2 x %d = %d\n", i, 2 * i); 
+  }
+
+  return 0;
+
+}
