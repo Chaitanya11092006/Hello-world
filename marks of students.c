f80eeb4855b3420976a32eb49ef2bace408caf10
chaitanya <chaitanyameshram119@gmail.com>
2025-07-18 16:35:19 +0530

    Create marks of students.c

---
+++ marks of students.c
@@ -0,0 +1,15 @@
+#include<studio.h>
+int main()
+{
+   int marks[32],i;
+   for(i=0;i<32;i++)
+   { 
+      printf("Enter marks of %d student\t:\n",i+1);
+      scanf("%d",&marks[i]);
+   }
+   fir(i=0;i<32;i++)
+   {
+    printf("Marks of %d are %d are %d\n",i+1, marks[i];
+   }
+   return 0;
+}
